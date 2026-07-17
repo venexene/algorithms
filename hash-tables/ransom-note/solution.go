703952fd72137cecf6af1869b24058799f9d2867
venexene <venexene@gmail.com>
2026-07-17 11:55:33 +0300

    feat: ransom-note

---
+++ hash-tables/ransom-note/solution.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	ransomNote := "aa"
+	magazine := "aab"
+	result := canConstruct(ransomNote, magazine)
+	fmt.Println(result)
+}
+
+func canConstructArr(ransomNote string, magazine string) bool {
+	arr := [26]int{}
+
+	for _, r := range magazine {
+		arr[r-"a"]++
+	}
+
+	for _, r := range ransomNote {
+		arr[r-"a"]--
+		if arr[r-"a"] < 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func canConstructMap(ransomNote string, magazine string) bool {
+	mp := map[rune]int{}
+
+	for _, r := range magazine {
+		mp[r]++
+	}
+
+	for _, r := range ransomNote {
+		mp[r]--
+		if mp[r] < 0 {
+			return false
+		}
+	}
+
+	return true
+}
