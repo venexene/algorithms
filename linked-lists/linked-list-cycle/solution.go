bea1334f953427767b66587bc8764678a4ecfa5e
venexene <venexene@gmail.com>
2026-07-26 19:53:24 +0300

    feat: add linked-list-cycle

---
+++ linked-lists/linked-list-cycle/solution.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	node1 := &ListNode{}
+	fmt.Println(node1)
+}
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+ 
+func hasCycle(head *ListNode) bool {
+	set := map[*ListNode]struct{}{}
+
+	for head != nil {
+		if _, ok := set[head]; ok {
+			return true
+		}
+		set[head] = struct{}{}
+		head = head.Next
+	}
+	return false
+}
+
+func hasCycleFloyd(head *ListNode) bool {
+	slow := head
+	fast := head
+
+	for fast != nil {
+		slow = slow.Next
+		fast = fast.Next
+		if fast == nil {
+			return false
+		}
+		fast = fast.Next
+
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
