6ea592f6399cbf56880029ebc6298d7c31b83622
venexene <venexene@gmail.com>
2026-07-26 12:21:32 +0300

    feat: add find-minimum-in-rotated-sorted-array

---
+++ binary-search/find-minimum-in-rotated-sorted-array/solution.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{5, 1, 2, 3, 4}
+	result := findMin(nums)
+	fmt.Println(result)
+}
+
+func findMin(nums []int) int {
+    low := 0
+	high := len(nums) - 1
+	
+	for low <= high {
+		mid := (low + high) / 2
+
+		if nums[mid] < nums[high] {
+			high = mid
+		} else if nums[mid] > nums[high] {
+			low = mid + 1
+		} else {
+			return nums[low]
+		}
+	}
+
+	return nums[low]
+}
