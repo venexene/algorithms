45ea5ed7bc1e15208891bacbbb2bd7cfc0a0cd16
venexene <venexene@gmail.com>
2026-07-29 19:54:31 +0300

    feat: add maximum-depth-of-binary-tree

---
+++ trees/maximum-depth-of-binary-tree/solution.go
@@ -0,0 +1,43 @@
+package main
+
+ type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	depth := 0
+	res := depth
+
+	if root == nil {
+		return 0
+	}
+
+	res = maxDepthRec(root, depth)
+
+	return res
+}
+
+func maxDepthRec(node *TreeNode, depth int) int {
+	depth++
+	res := depth
+
+	if node.Left != nil {
+		res = max(res, maxDepthRec(node.Left, depth))
+	}
+
+	if node.Right != nil {
+		res = max(res, maxDepthRec(node.Right, depth))
+	}
+
+	return res
+}
+
+func maxDepthShort(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
