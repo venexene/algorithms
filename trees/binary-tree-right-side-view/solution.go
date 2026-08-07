0910e3f61ec9cf7c69d93693f0c1560359fb6168
venexene <venexene@gmail.com>
2026-08-07 21:14:42 +0300

    feat: add binary-tree-right-side-view

---
+++ trees/binary-tree-right-side-view/solution.go
@@ -0,0 +1,34 @@
+package main
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func rightSideView(root *TreeNode) []int {
+    if root == nil {
+		return []int{}
+	}
+
+	res := []int{}
+	max := 0
+	rightSideViewAdd(root, &res, 0, &max)
+	return res
+}
+
+func rightSideViewAdd(node *TreeNode, view *[]int, depth int, max *int) {
+    depth++
+	if depth > *max {
+		*view = append(*view, node.Val)
+		*max = depth
+	}
+
+	if node.Right != nil {
+		rightSideViewAdd(node.Right, view, depth, max)
+	}
+	
+	if node.Left != nil {
+		rightSideViewAdd(node.Left, view, depth, max)
+	}
+}
