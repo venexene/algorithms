75631fbc6504ba2f3b338a8c1a7bfcd06781ff15
venexene <venexene@gmail.com>
2026-08-09 19:58:08 +0300

    feat: add maximum-subarray

---
+++ dynamic-programming/maximum-subarray/solution.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{5,4,-1,7,8}
+	result := maxSubArray(nums)
+	fmt.Println(result)
+}
+
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	sum := nums[0]
+	for i := 1; i < len(nums); i++ {
+		nums[i] = max(nums[i], nums[i] + nums[i-1])
+		sum = max(nums[i], sum)
+	}
+
+	return sum
+}
