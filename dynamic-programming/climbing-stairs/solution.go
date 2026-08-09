40b6dd124afbad85f17389ce253c255338d8b19c
venexene <venexene@gmail.com>
2026-08-09 17:48:14 +0300

    feat: add climbing-stairs

---
+++ dynamic-programming/climbing-stairs/solution.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 2
+	result := climbStairs(n)
+	fmt.Println(result)
+}
+
+func climbStairs(n int) int {
+	s1 := 1
+	s2 := 1
+    for i := 0; i < n; i++ {
+		s1, s2 = s2, s1+s2
+	}
+	return s2
+}
