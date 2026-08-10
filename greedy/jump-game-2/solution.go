11777536b98f9f01fe50928e6e9652f0f9a16140
venexene <venexene@gmail.com>
2026-08-10 19:35:10 +0300

    feat: add jump-game-2

---
+++ greedy/jump-game-2/solution.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2,3,0,1,4}
+	result := jump(nums)
+	fmt.Println(result)
+}
+
+func jump(nums []int) int {
+	mx := nums[0]
+	mx2 := nums[0]
+	res := 1
+	
+	if len(nums) == 1 {
+		return 0
+	}
+	for i := 1; i < len(nums); i++ {
+		if mx >= len(nums) - 1 {
+			return res
+		}
+
+		if mx < i {
+			res++
+			mx = mx2
+		}
+		mx2 = max(mx2, i + nums[i])
+	}
+
+	return res
+}
