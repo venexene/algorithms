e2dc0407aee740ad69b1236cd84f66735a8391a2
venexene <venexene@gmail.com>
2026-08-14 19:35:58 +0300

    feat: add combination-sum

---
+++ backtracking/combination-sum/solution.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	candidates := []int{2, 3, 5}
+	target := 8
+	result := combinationSum(candidates, target)
+	fmt.Println(result)
+}
+
+func combinationSum(candidates []int, target int) [][]int {
+    result := [][]int{}
+	slices.Sort(candidates)
+	combinationSumAdd(candidates, target, 0, []int{}, &result)
+	return result
+}
+
+func combinationSumAdd(nums []int, target, start int, set []int, res *[][]int) {
+	if target == 0 {
+		*res = append(*res, append([]int{}, set...))
+		return
+	}
+	if target < 0 {
+		return
+	}
+
+	for i := start; i < len(nums); i++ {
+		if nums[i] > target {
+			break
+		}
+		set = append(set, nums[i])
+		combinationSumAdd(nums, target - nums[i], i, set, res)
+		set = set[:len(set)-1]
+	}
+}
